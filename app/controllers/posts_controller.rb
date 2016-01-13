f9165f8acad521134d07ea87e683f6bf1965af82
jen pengelly <jgpengelly@gmail.com>
2016-01-13 08:45:57 -0500

    updates to carrierwave and integration into form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,18 +42,15 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    @post.update(post_params)
-    redirect_to @post
-
-    # respond_to do |format|
-    #   if @post.update(post_params)
-    #     format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-    #     format.json { render :show, status: :ok, location: @post }
-    #   else
-    #     format.html { render :edit }
-    #     format.json { render json: @post.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to posts_path, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: posts_path }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # DELETE /posts/1
